0d60f1e149b1699a86339bebafd246446a55679b
Jonas Curth <mail@jonascurth.de>
2022-10-18 22:54:42 +0200

    Initial Website just to serve assetlinks.json

---
+++ Website/ClientApp/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <p>This Website is still under construction, please come back later.</p>
+      </div>
+    );
+  }
+}
---
+++ Website/Controllers/WellKnownController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+    [Route(".well-known")]
+    [ApiController]
+    public class WellKnownController : ControllerBase
+    {
+        [HttpGet("assetlinks.json")]
+        public IActionResult GetAssetLinks()
+        {
+            return this.Ok(new
+            {
+                releation = new string[] { "delegate_permission/common.handle_all_urls" },
+                target = new
+                {
+                    @namespace = "android_app",
+                    package_name = "dev.curth.hero",
+                    sha256_cert_fingerprints = new string[] { "4D:5E:D1:84:CA:FA:B6:75:B0:E7:4B:43:EB:D4:9E:46:BD:6A:F5:B7:DD:9C:46:66:A3:E8:0E:68:24:92:EE:C2" }
+                }
+            });
+        }
+    }
+}
